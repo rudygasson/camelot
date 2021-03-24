a35a39ead5786eeee808ed2a3011740c5754727c
Rudy Gasson <2715405+rudygasson@users.noreply.github.com>
2021-03-24 03:07:47 +0100

    Initial commit: Setup svelte with header

---
+++ src/App.svelte
@@ -0,0 +1,65 @@
+<script>
+import { schedule_update } from 'svelte/internal';
+
+	import Header from './UI/Header.svelte';
+
+	let isLoading = true;
+	let scData = {};
+	let firstName = '';
+	let error;
+
+	fetch("sc.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error("Ups, ein Fehler. Bitte versuche es noch einmal.");
+            }
+            return res.json();
+        })
+        .then((data) => {
+			console.log(data);
+            scData = data;
+			firstName = scData.Name.split(' ')[0];
+            isLoading = false;
+        })
+
+        .catch((err) => {
+            error = err;
+            isLoading = false;
+            console.warn(err);
+        });
+</script>
+
+<main>
+	{#if isLoading}
+		<p>Loading...</p>
+	{:else}
+		<Header 
+			name={scData.Name} 
+			lifePoints={scData.Lebenspunkte} 
+			stamina= {scData.Ausdauer}
+			magicPower= {scData.Astralenergie}/>
+		<h1>Hallo {firstName}!</h1>
+	{/if}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 3em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/UI/Header.svelte
@@ -0,0 +1,52 @@
+<script>
+    import Life from "./Life.svelte";
+    import Stamina from "./Stamina.svelte";
+    import Magic from "./Magic.svelte";
+
+    export let name = "";
+    export let lifePoints = 0;
+    export let stamina = 0;
+    export let magicPower =0;
+
+    let firstName = name.split(" ")[0];
+    let [lastName] = name.split(" ").slice(-1);
+</script>
+
+<style>
+    header {
+        padding: 0.5rem;
+        position: fixed;
+        width: 100%;
+        top: 0;
+        left: 0;
+        height: 3rem;
+        background: #26794f;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.26);
+    }
+    div {
+        display: flex;
+        align-items: center;
+    }
+    h1 {
+        color: white;
+        margin: 0;
+    }
+</style>
+
+<svelte:head>
+    <title>{firstName} {lastName}</title>
+</svelte:head>
+
+<header>
+    <div>
+        <Life {lifePoints} />
+        <Stamina {stamina} />
+    </div>
+    <h1>{firstName}</h1>
+    <div><Magic {magicPower} /></div>
+</header>
+
+

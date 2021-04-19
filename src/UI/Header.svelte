546fa5b742dc8e26daf4030f6e9470cef02698df
Rudy Gasson <2715405+rudygasson@users.noreply.github.com>
2021-04-19 04:43:11 +0200

    Refactor Header

--- src/UI/Header.svelte
+++ src/UI/Header.svelte
@@ -1,53 +1,46 @@
 <script>
-    import Life from "./Life.svelte";
-    import Stamina from "./Stamina.svelte";
-    import Magic from "./Magic.svelte";
+  import HeaderScore from "./HeaderScore.svelte";
 
-    export let name = "";
-    export let lifePoints = 0;
-    export let stamina = 0;
-    export let magicPower = 0;
-    export let magicResistence = 0;
+  export let name = "";
+  export let lifePoints = 0;
+  export let stamina = 0;
+  export let magicPower = 0;
+  export let magicResistence = 0;
 
-    let firstName = name.split(" ")[0];
-    let [lastName] = name.split(" ").slice(-1);
+  let firstName = name.split(" ")[0];
+  let [lastName] = name.split(" ").slice(-1);
 </script>
 
 <svelte:head>
-    <title>{firstName} {lastName}</title>
+  <title>{firstName} {lastName}</title>
 </svelte:head>
 
 <header>
-    <div>
-        <Life {lifePoints} />
-        <Stamina {stamina} />
-    </div>
-    <h2>{firstName}</h2>
-    <div><Magic {magicPower} {magicResistence}/></div>
+  <HeaderScore icon="heart" score={lifePoints} />
+  <HeaderScore icon="hourglass" score={stamina} />
+  <div class="header__title">{firstName}</div>
+  <HeaderScore icon="magic-wand" score={magicPower} />
+  <HeaderScore icon="bolt" score={magicResistence} />
 </header>
 
 <style>
-    header {
-        padding: 0.5rem;
-        position: fixed;
-        width: 100%;
-        top: 0;
-        left: 0;
-        height: 3rem;
-        background: #6d1919;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.26);
-    }
-    div {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        width: 7.5rem;
-    }
-    h2 {
-        color: white;
-        margin: auto;
-    }
+  header {
+    color: white;
+    padding: 0.5rem;
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+    height: 3rem;
+    background: #6d1919;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .header__title {
+    margin: auto;
+    height: 2rem;
+    font-size: 1.5rem;
+  }
 </style>
---
+++ src/UI/HeaderScore.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let icon = "";
+  export let score = 0;
+</script>
+
+<div role="cell" class="score">
+  <img class="score__icon" src={"assets/icons/" + icon + ".svg"} alt="" />
+  <p class="score__value">{score}</p>
+</div>
+
+<style>
+  .score {
+    height: 2rem;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 4.4rem;
+  }
+
+  .score__icon {
+    height: 2rem;
+  }
+
+  .score__value {
+    font-size: 1.5rem;
+  }
+</style>
